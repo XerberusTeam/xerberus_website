76db6f2d8395f2f2f016bdb5f90d84c89bb6a74e
G.M <mg@xerberus.io>
2022-08-30 12:08:36 +0200

    add light paper

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,18 +6,20 @@ function Hero() {
     <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover xrb-bg-img">
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]"></div>
       <div className="p-5 text-white z-[2]">
-        <h2 className="text-5xl font-bold ">Wallet Watcher</h2>
-        <p className="py-5 text-xl ">Your Risk Manager for Cardano Tokens</p>
+        <h2 className="text-5xl font-bold text-center">Wallet Watcher</h2>
+        <p className="py-5 text-xl text-center">
+          Your Risk Manager for Cardano Tokens
+        </p>
         <Link href="https://discord.gg/zSDQNaH5">
           <a target="_blank">
-            <button className="px-8 py-2 border w-full bg-white text-black text-2xl font-bold">
+            <button className="py-2 border w-full bg-white text-black text-2xl font-bold">
               Join the Discord
             </button>
           </a>
         </Link>
         <Link href="https://drive.google.com/file/d/1hE8EwQAeC8Dspka8DN3HRmcuBJAqnRMM/view">
           <a target="_blank">
-            <button className="px-8 py-2 mt-5 border w-full bg-transparent text-white text-2xl font-bold">
+            <button className="py-2 mt-5 border w-full bg-transparent text-white text-2xl font-bold">
               Light Paper 001
             </button>
           </a>
